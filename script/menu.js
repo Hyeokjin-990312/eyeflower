b07d2673ccfb1b24aea264b56c18b11c26cfd7bc
Hyeokjin-990312 <108320475+Hyeokjin-990312@users.noreply.github.com>
2022-07-12 16:11:58 +0900

    Delete menu.js

--- script/menu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.nav1> li').mouseover(function(){
-        $(this).children('.sub').stop().slideDown();
-    });
-    $('.nav1> li').mouseout(function(){
-        $(this).children('.sub').stop().slideUp();
-    });
-});
